0eb95bd9fb264fa2835c27f3cd94945e41c66de4
yihengcai0006 <ucapyca@ucl.ac.uk>
2025-10-19 18:05:31 +0100

    Modified Step 1 and Step 2 in readme

--- earthquakes.py
+++ earthquakes.py
@@ -1,5 +1,4 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
+# The Python standard library includes some functionality for communicating over the Internet.
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
 import json
